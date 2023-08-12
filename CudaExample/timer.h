f3644732466e9d7d20d432440ad633cdc8bc6a89
Jooh <mnh513@naver.com>
2023-08-12 23:24:26 +0900

    vector addtion with block + scoped timer implementation

---
+++ CudaExample/timer.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+class Timer
+{
+private:
+	static Timer instance;
+
+public:
+	static Timer& getInstance() {
+		return instance;
+	}
+
+	void addRecord(string name, double time) {
+		names.push_back(name);
+		counters.push_back(time);
+	}
+
+	void printRecord() {
+		int size = names.size();
+		printf("---- Timer ----\n");
+		for (int i = 0; i < size; i++) {
+			printf("%s - %lf ms\n", names[i].c_str(), counters[i]);
+		}
+	}
+	vector<string> names;
+	vector<double> counters;
+};
+
+Timer Timer::instance;
+
+#define SCOPED_TIMER(Name) ScopedTimer t1 = ScopedTimer(Name);
+
+class ScopedTimer
+{
+private:
+	string name;
+	steady_clock::time_point start;
+
+public:
+	ScopedTimer(string inName)
+	{
+		start = steady_clock::now();
+		name = inName;
+	}
+
+	~ScopedTimer()
+	{
+		duration<double> sec = (steady_clock::now() - start);
+		Timer::getInstance().addRecord(name, sec.count() * 1000);
+	}
+};
---
+++ CudaExample/vector_addition_with_block.cu
@@ -0,0 +1,97 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "timer.h"
+
+// The size of the vector
+#define NUM_DATA 102490000
+
+// Simple vector sum kernel (Max vector size : 1024)
+__global__ void vecAddBlock(int* _a, int* _b, int* _c, int _size) {
+	int tID = threadIdx.x;
+	int bID = blockIdx.x;
+	int ID = tID + bID * 1024;
+	if (ID < _size)
+		_c[ID] = _a[ID] + _b[ID];
+}
+
+int main(void)
+{
+	int* a, * b, * c, * hc;	// Vectors on the host
+	int* da, * db, * dc;	// Vectors on the device
+
+	int memSize = sizeof(int) * NUM_DATA;
+	printf("%d elements, memSize = %d bytes\n", NUM_DATA, memSize);
+
+	// Memory allocation on the host-side
+	a = new int[NUM_DATA]; memset(a, 0, memSize);
+	b = new int[NUM_DATA]; memset(b, 0, memSize);
+	c = new int[NUM_DATA]; memset(c, 0, memSize);
+	hc = new int[NUM_DATA]; memset(hc, 0, memSize);
+
+	// Data generation
+	for (int i = 0; i < NUM_DATA; i++) {
+		a[i] = rand() % 10;
+		b[i] = rand() % 10;
+	}
+
+	// Vector sum on host (for performance comparision)
+	{
+		SCOPED_TIMER("vecAddBlock CPU");
+		for (int i = 0; i < NUM_DATA; i++)
+			hc[i] = a[i] + b[i];
+	}
+
+	// Memory allocation on the device-side
+	{
+		SCOPED_TIMER("cudaMalooc&Memcpy");
+		cudaMalloc(&da, memSize); cudaMemset(da, 0, memSize);
+		cudaMalloc(&db, memSize); cudaMemset(db, 0, memSize);
+		cudaMalloc(&dc, memSize); cudaMemset(dc, 0, memSize);
+
+		// Data copy : Host -> Device
+		cudaMemcpy(da, a, memSize, cudaMemcpyHostToDevice);
+		cudaMemcpy(db, b, memSize, cudaMemcpyHostToDevice);
+	}
+
+	// Kernel call
+	{
+		SCOPED_TIMER("vecAddBlock kernel call");
+		vecAddBlock <<< ceil((float)NUM_DATA / 1024), 1024 >> > (da, db, dc, NUM_DATA);
+		cudaDeviceSynchronize();
+	}
+
+	{
+		SCOPED_TIMER("cudaMemcpy&cudaFree");
+
+		// Copy results : Device -> Host
+		cudaMemcpy(c, dc, memSize, cudaMemcpyDeviceToHost);
+
+		// Release device memory
+		cudaFree(da); cudaFree(db); cudaFree(dc);
+	}
+
+	// Check results
+	bool result = true;
+	for (int i = 0; i < NUM_DATA; i++) {
+		if (hc[i] != c[i]) {
+			printf("[%d] The result is not matched! (%d, %d)\n"
+				, i, hc[i], c[i]);
+			result = false;
+		}
+	}
+
+	if (result)
+		printf("GPU works well!\n");
+
+	// Release host memory
+	delete[] a; delete[] b; delete[] c;
+
+	Timer::getInstance().printRecord();
+
+	return 0;
+}
